76017e9ad618dd3f4489ff638c9aa6f3996b1ab5
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-07-10 07:21:37 +0100

    chore: rendering image on the screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>Health</div>
+   <div className="flex h-screen max-h-screen">
+    <section className="remove-scrollbar container my-auto">
+      <div className="sub-container max-w-[496px]">
+       <Image src="/assets/icons/logo-full.svg"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="mb-12 h-10 w-fit"
+       />
+      </div>
+    </section>
+    </div>
   );
 }
