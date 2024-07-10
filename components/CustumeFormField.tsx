3c2178ed8e3b19e8907707a42e1cabaece108331
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-07-10 23:54:54 +0100

    chore: create a short function for field

--- components/CustumeFormField.tsx
+++ components/CustumeFormField.tsx
@@ -13,6 +13,7 @@ import {
 import { Control } from "react-hook-form"
 import { FormFieldtype } from "./forms/PatientForm"
 import { Label } from "@radix-ui/react-label"
+import { Image } from "lucide-react"
 
 
 interface CustomProps {
@@ -31,12 +32,39 @@ interface CustomProps {
 }
 
 const RenderInput = ({field,props}:{field:any ,props:CustomProps}) =>{
-  return(
-    <Input 
-    type="text"
-    placeholder="Felix Mavila"
-    />
-  )
+  const {fieldType,iconSrc,iconAlt,placeholder} = props;
+  
+    switch (props.fieldType){
+    case FormFieldtype.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+         {iconSrc && (
+          <Image 
+          src={iconSrc}
+          height={24}
+          width={24}
+          alt={iconAlt || 'icon'}
+          className="ml-24"
+          />
+         )}
+
+         <FormControl>
+          <Input 
+          
+          placeholder={placeholder}
+          {...field}
+          className="shad-input border-0"
+          />
+         </FormControl>
+        </div>
+      )
+
+      default:
+        break;
+    
+    }
+    
+  
 }
 
 const CustumeFormField = (props: CustomProps) => {
