aa121ec5e59f0e14e1e5afff1d162849d4470ee2
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-07-23 05:24:59 +0100

    chore: define config appwrite in my app

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,5 +1,5 @@
 "use client"
- 
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -9,83 +9,91 @@ import CustumeFormField from "../CustumeFormField"
 import { SubmitButton } from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
-
+import { useRouter } from "next/navigation"
 
 export enum FormFieldtype {
     INPUT = 'input',
     TEXTAREA = 'textarea',
     PHONE_INPUT = 'phoneInput',
-    CHECKBOX='checkbox',
+    CHECKBOX = 'checkbox',
     DATE_PICKER = 'datePicker',
     SELECT = 'select',
     RADIO = 'radio',
     SKELETON = 'skeleton'
 }
- 
 
- 
 const PatientForm = () => {
-  const [isLoading,setIsLoading] = useState(false);
+  const router =useRouter(); 
+  const [isLoading, setIsLoading] = useState(false);
+ 
   // 1. Define your form.
   const form = useForm<z.infer<typeof UserFormValidation>>({
     resolver: zodResolver(UserFormValidation),
     defaultValues: {
       name: "",
-      email:"",
-      phone:""
+      email: "",
+      phone: ""
     },
-  })
- 
+  });
+
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof UserFormValidation>) {
+ async function onSubmit({name,email,phone}: z.infer<typeof UserFormValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    setIsLoading(true)
+    setIsLoading(true);
+
+    try{
+      //const userData = {name,email,phone}
+      //const user = await createUser(userData);
+
+      //if(user) router.push(`./patients/${user.$id}/register`)
+
+    }catch(error){
+     console.log(error)
+    }
   }
 
   return (
-   
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
         <section className="mb-12 space-y-4">
           <h1 className="header">Cuide da Sua Saúde Connosco</h1>
           <p className="text-dark-700">Agende a sua primeira consulta</p>
         </section>
-        <CustumeFormField 
-        control={form.control}
-        fieldType={FormFieldtype.INPUT}
-        name="name"
-        label="Full name"
-        placeholder="John Doe"
-        iconSrc="/assets/icons/user.svg"
-        iconAlt="user"
+        <CustumeFormField
+          control={form.control}
+          fieldType={FormFieldtype.INPUT}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
         />
 
-      <CustumeFormField 
-        control={form.control}
-        fieldType={FormFieldtype.INPUT}
-        name="email"
-        label="Email"
-        placeholder="felixmacarenco@gmail.com"
-        iconSrc="/assets/icons/email.svg"
-        iconAlt="email"
+        <CustumeFormField
+          control={form.control}
+          fieldType={FormFieldtype.INPUT}
+          name="email"
+          label="Email"
+          placeholder="felixmacarenco@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
         />
 
-      <CustumeFormField 
-        control={form.control}
-        fieldType={FormFieldtype.PHONE_INPUT}
-        name="phone"
-        label="Phone"
-        placeholder="929374017"
-        iconSrc="/assets/icons/phone.svg"
-        iconAlt="phone"
+        <CustumeFormField
+          control={form.control}
+          fieldType={FormFieldtype.PHONE_INPUT}
+          name="phone"
+          label="Phone"
+          placeholder="929374017"
+          iconSrc="/assets/icons/phone.svg"
+          iconAlt="phone"
         />
-        
-      <SubmitButton isLoading={isLoading}>Registrar-se</SubmitButton>
 
+        <SubmitButton isLoading={isLoading}>Registrar-se</SubmitButton>
       </form>
     </Form>
-  )
+  );
 }
 
-export default PatientForm
+export default PatientForm;
---
+++ lib/appwrite.config.ts
@@ -0,0 +1,23 @@
+import * as sdk from 'node-appwrite' 
+
+const {
+PROJECT_ID,
+API_KEY,
+DATABASE_ID,
+PATIENT_COLLECTION_ID,
+DOCTOR_COLLECTION_ID,
+APPOINTMENT_COLLECTION_ID,
+STORAGE_BUCKET:STORAGE_BUCKET_ID,
+NEXT_PUBLIC:NEXT_PUBLIC_ENDPOINT,
+} = process.env;
+
+const client = new sdk.Client();
+
+client.setEndpoint(NEXT_PUBLIC_ENDPOINT!)
+.setProject(PROJECT_ID!)
+.setKey(API_KEY!)
+
+export const databases = new sdk.Databases(client)
+export const storage = new sdk.Storage(client)
+export const messaging = new sdk.Messaging(client)
+export const users = new sdk.Users(client)
